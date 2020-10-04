fa05f09810d930fa10707e4cc49a0e6bd62ab7de
DCartist <pbannerman13@gmail.com>
2020-10-03 23:14:23 -0400

    added random color background

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import "./Home.css"
+const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
 
 class Home extends Component {
+    
     render() {
+        // console.log(randomColor)
         return (
-            <div className="homeBase">
+            <div className="homeBase" style={{backgroundColor: randomColor}}>
                 <h1>COLOR AND DEISGN</h1>
             </div>
         );
