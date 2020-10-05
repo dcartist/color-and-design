99609e92e52d335c3c3ce4897cb957a8b1b7be3c
DCartist <pbannerman13@gmail.com>
2020-10-04 21:12:43 -0400

    adding search by first character

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css"
 import {Route, Link, Switch, Redirect} from "react-router-dom";
 import Home from './Home/Home'
 import Colors from './Colors/Colors'
+import ColorsPage from './Colors/ColorPage'
+import ColorsAlphabet from './Colors/ColorPageAlphabet'
 // import PropTypes from 'prop-types';
 
 class App extends Component {
@@ -18,6 +20,10 @@ class App extends Component {
       </Route>
         <Route exact path="/colors">
         <Colors />
+      </Route>
+        <Route exact path="/colors/:pageNumber" component={ColorsPage}>
+      </Route>
+        <Route exact path="/search/digit/:digit" component={ColorsAlphabet}>
       </Route>
       </div>
     );
---
+++ src/Colors/ColorPage.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Modal from "../Modal/Modal"
+import Cards from "../Card/CardCusomDesign"
+import axios from 'axios';
+import "./Color.css"
+class ColorPage extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+          color:[]
+        }
+      }
+    componentDidMount(){
+    console.log(this.props.match.params.pageNumber)
+    console.log("testing")
+    let url = `https://flask.colorand.design/colors/full/${this.props.match.params.pageNumber}`
+        axios.get(url).then(
+            results=> {
+                this.setState({color:results.data})
+                console.log(this.state.color)
+                console.log(results.data)
+                console.log("test")
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className="colorBase">
+                {this.state.color.map((item, index)=>(
+                    <Cards {...item} key={index}></Cards>
+                ))}
+            </div>
+        );
+    }
+}
+
+
+export default ColorPage;
