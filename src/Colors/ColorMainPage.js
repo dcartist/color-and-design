1f53ac8e7c5cd8ccf783a09f00b5489f7392cb93
DCartist <pbannerman13@gmail.com>
2020-10-09 13:17:33 -0400

    Move pagination to center of page

--- src/Colors/ColorMainPage.js
+++ src/Colors/ColorMainPage.js
@@ -3,12 +3,12 @@ import PaginationActive from '../Pagination/PaginationActive'
 import Modal from '../Modal/Modalboot'
 import FullColor from '../Colors/ColorFullSelector'
 import axios from 'axios';
+import "./Color.css"
 
 class ColorMainPage extends Component {
  state = { activePage: 0,
     activeSelection:"",
     color:[] }
-//   handleInputChange = (e, { value }) => this.setState({ activePage: value })
   handleInputChange = (e, { value }) => {
     e.preventDefault();
     this.setState({ activePage: value })
@@ -37,20 +37,13 @@ class ColorMainPage extends Component {
         )
 
   }
-//   handlePaginationChange = (e, { activePage }) => this.setState({ activePage })
-//   handlePaginationChange = (e, { activePage }) => this.setState({ activePage })
 
   componentDidMount(){
-    // console.log(this.props.match.params.pageNumber)
-    // console.log(this.props.location.pathname)
     console.log("testing")
     let url = `https://flask.colorand.design/colors/full/${this.state.activePage}`
         axios.get(url).then(
             results=> {
                 this.setState({color:results.data})
-                console.log(this.state.color)
-                console.log(results.data)
-                console.log("test")
             }
         )
     }
@@ -58,7 +51,10 @@ class ColorMainPage extends Component {
     render() {
         return (
             <div>
-                <PaginationActive activePage={this.state.activePage} handleInputChange={this.handleInputChange} handlePaginationChange={this.handlePaginationChange}></PaginationActive>
+                <div className="PaginationCenter">
+                <PaginationActive activePage={this.state.activePage} handleInputChange={this.handleInputChange} handlePaginationChange={this.handlePaginationChange} ></PaginationActive>
+                </div>
+                
                 <FullColor activeSelection={this.state.activePage} color={this.state.color}></FullColor>
 
                 {/* <Modal></Modal> */}
