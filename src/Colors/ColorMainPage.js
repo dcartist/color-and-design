f2c4b8bd822e2bc5a4bdb5bff0065773836847da
DCartist <pbannerman13@gmail.com>
2020-10-18 12:48:48 -0400

    color search updated

--- src/Colors/ColorMainPage.js
+++ src/Colors/ColorMainPage.js
@@ -1,136 +1,88 @@
 import React, { Component } from 'react';
-import PaginationActive from '../Pagination/PaginationActive'
-import Modal from '../Modal/Modalboot'
-import Loader from "../Loader/Loader"
-import FullColor from '../Colors/ColorFullSelector'
+import PaginationActive from '../Pagination/PaginationActive';
+import Loader from '../Loader/Loader';
+import FullColor from '../Colors/ColorFullSelector';
 import axios from 'axios';
-
-import "./Color.css"
+import './Color.css';
 
 class ColorMainPage extends Component {
- state = { activePage: 0,
-    searchSelected: false,
-    searchInput: "",
-    activeSelection:"",
-    showEllipsis: true,
-    showFirstAndLastNav: true,
-    showPreviousAndNextNav: true,
-    color:[] }
-  handleSearch = () =>{
-    //   this.setState({ searchSelected: false })
-    //   console.log(e.value)
-    //   let url = `https://flask.colorand.design/colors/full/${urlPage}`
-    let url = `https://flask.colorand.design/colors/snippet/${this.state.searchInput}`
-    console.log(url)
-    axios.get(url).then(
-        results=> {
-            this.setState({color:results.data})
-            console.log(this.state.color[0].id)
-            console.log(results.data)
-            console.log(`Post pagination active page change to ${this.state.activePage} URL is ${url}`)
-            // console.log(this.state.activePage)
-        }
-    )
-  }
-  handleInput = (e) =>{
-    e.preventDefault()
-    console.log(e.target.value)
-    this.setState({ searchSelected: false, searchInput: e.target.value })
-    
+	state = {
+		activePage: 0,
+		searchSelected: false,
+		searchInput: '',
+		activeSelection: '',
+		showEllipsis: true,
+		showFirstAndLastNav: true,
+		showPreviousAndNextNav: true,
+		color: [],
+	};
+	handleSearch = () => {
+		let url = `https://flask.colorand.design/colors/snippet/${this.state.searchInput}`;
+		console.log(url);
+		axios.get(url).then((results) => {
+			this.setState({ color: results.data });
+			console.log(this.state.color[0].id);
+			console.log(results.data);
+			console.log(`Post pagination active page change to ${this.state.activePage} URL is ${url}`);
+		});
+	};
+	handleInput = (e) => {
+		// e.preventDefault()
+		this.setState({ searchSelected: false, searchInput: e.target.value });
+	};
+	handlePaginationChange = (e, { activePage }) => {
+		// e.preventDefault()
+		let urlPage = activePage - 1;
+		this.setState({ activePage: activePage, searchSelected: true });
+
+		let url = `https://flask.colorand.design/colors/full/${urlPage}`;
+		console.log(`before axios call active page change to ${this.state.activePage} URL is ${url}`);
+		axios.get(url).then((results) => {
+			this.setState({ color: results.data });
+			console.log(this.state.color[0].id);
+			console.log(results.data);
+			console.log(`Post pagination active page change to ${this.state.activePage} URL is ${url}`);
+		});
+	};
+
+	componentDidMount() {
+		console.log('testing');
+		let url = `https://flask.colorand.design/colors/full/${this.state.activePage}`;
+		axios.get(url).then((results) => {
+			this.setState({ color: results.data });
+			console.log(`Post Mounted active page change to ${this.state.activePage} URL is ${url} `);
+		});
+	}
+
+	render() {
+		if (this.state.color.length == 0) {
+			return (
+				<div className="PageLoader">
+					<Loader />
+				</div>
+			);
+		} else {
+			return (
+				<div>
+                    <div className="SearchFunction">
+							<form>
+								<input type="text" onChange={this.handleInput} />
+							</form>
+							<button onClick={this.handleSearch}>Click here</button>
+						</div>
+					<div className="PaginationCenter">
+						<PaginationActive
+							activePage={this.state.activePage}
+							handleInputChange={this.handleInputChange}
+							handlePaginationChange={this.handlePaginationChange}
+						/>
+					</div>
+
+					<FullColor activeSelection={this.state.activePage} color={this.state.color} />
+				</div>
+			);
+		}
+	}
 }
-  handlePaginationChange = (e, { activePage }) => {
-    // e.preventDefault();
-    let urlPage = activePage - 1
-    this.setState({ activePage: activePage, searchSelected:true })
-    
-    let url = `https://flask.colorand.design/colors/full/${urlPage}`
-    console.log(`before axios call active page change to ${this.state.activePage} URL is ${url}`)
-        axios.get(url).then(
-            results=> {
-                this.setState({color:results.data})
-                console.log(this.state.color[0].id)
-                console.log(results.data)
-                console.log(`Post pagination active page change to ${this.state.activePage} URL is ${url}`)
-                // console.log(this.state.activePage)
-            }
-        )
-
-  }
-
-  componentDidMount(){
-    console.log("testing")
-    let url = `https://flask.colorand.design/colors/full/${this.state.activePage}`
-        axios.get(url).then(
-            results=> {
-                this.setState({color:results.data})
-                console.log(`Post Mounted active page change to ${this.state.activePage} URL is ${url} `)
-            }
-        )
-    }
-
-    render() {
-        if (this.state.color.length == 0){
-            return(
-                <div className="PageLoader">
-                    <Loader></Loader>
-                </div>
-            )
-        } else {
-            
-            return (
-                <div>
-                    <form>
-                            <input type="text" onChange={this.handleInput}></input>
-                        </form>
-                            <button onClick={this.handleSearch}>Click here</button>
-                    <div className="PaginationCenter">
-                        
-                    <PaginationActive activePage={this.state.activePage} handleInputChange={this.handleInputChange} handlePaginationChange={this.handlePaginationChange} ></PaginationActive>
-                    </div>
-                    
-                    
-                    <FullColor activeSelection={this.state.activePage} color={this.state.color}></FullColor>
-                </div>
-            );
-            
-            // if (this.state.searchSelected) {
-            //     return (
-            //         <div>
-            //             <form>
-            //                     <input type="text" onChange={this.handleInput}></input>
-            //                 </form>
-            //             <div className="PaginationCenter">
-                            
-            //             <PaginationActive activePage={this.state.activePage} handleInputChange={this.handleInputChange} handlePaginationChange={this.handlePaginationChange} ></PaginationActive>
-            //             </div>
-                        
-                        
-            //             <FullColor activeSelection={this.state.activePage} color={this.state.color}></FullColor>
-            //         </div>
-            //     );
-            // } else{
-    
-            //     return (
-            //         <div>
-            //             FALSE
-            //              <form>
-            //                     <input type="text" onChange={this.handleInput}></input>
-            //                 </form>
-            //             <div className="PaginationCenter">
-            //             <PaginationActive activePage={this.state.activePage} handleInputChange={this.handleInputChange} handlePaginationChange={this.handlePaginationChange} ></PaginationActive>
-            //             </div>
-    
-            //             <FullColor activeSelection={this.state.activePage} color={this.state.color}></FullColor>
-            //         </div>
-            //     );
-            // }
-            // return 
-        }
-        
-        
-        
-    }
-}
-
 
 export default ColorMainPage;
