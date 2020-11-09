f98264abfa160fbc6c15c2f10ba72a6b28d96a04
DCartist <pbannerman13@gmail.com>
2020-11-09 11:35:20 -0500

    updated about section

--- src/About/About.js
+++ src/About/About.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import "./About.css"
 
 class About extends Component {
     render() {
         return (
-            <div>
+            <div className="aboutBase">
                <p>Color and Design is an app built with React for the front end and Flask for the backend. Out of millions of colors, this app only lists around 25,000 color names using a dataset(JSON).</p>
-               <p>To view API via swagger:<a href="https://api.colorand.design/swagger/" target="_blank">https://api.colorand.design/swagger/</a>
+               <p>To view API via swagger: <a href="https://api.colorand.design/swagger/" target="_blank">https://api.colorand.design/swagger/</a>
 </p>
                <p>Notes:
 The Flask API was created in 6 hours during a hackathon by Paula Bannerman. It is deployed serverless on vercel.com.
